4fc327033bfc4610e41ac1dfd7c66cad8d74fce3
Aulianyb <18221066@std.stei.itb.ac.id>
2025-03-20 16:52:12 +0700

    feat : layout landing page

---
+++ src/components/navbar.tsx
@@ -0,0 +1,11 @@
+import ColoredLogo from "../assets/ColoredLogo.png";   
+
+export default function Navbar() {
+  return (
+    <>
+      <div>
+          <img src={ColoredLogo} alt="Colored Logo" className="w-40"/>
+      </div>
+    </>
+  );
+}
