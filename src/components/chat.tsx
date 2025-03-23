729bcfb93ebb3fe66ff2832817ec8567d6d82d63
rayhanmp <rayhanmpramanda@gmail.com>
2025-03-23 19:06:13 +0700

    feat(consultation): integrated with BE, more polished UI

--- src/components/chat.tsx
+++ src/components/chat.tsx
@@ -1,9 +1,11 @@
 export default function Chat({message} : {message : String}) {
   return (
     <>
-      <div className="bg-zinc-300 p-2 w-lg text-right rounded-md self-end">
+    <div className="flex justify-end w-full">
+      <div className="inline-block bg-blue-200 px-3 py-2 rounded-md text-right max-w-fit">
         {message}
       </div>
+    </div>
     </>
   );
 }
